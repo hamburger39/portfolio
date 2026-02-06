ff943853bdb2db6afb01cc2023bfee40574e4e4a
hamburger39 <khhira3229@icloud.com>
2026-02-06 16:37:41 +0900

    Update Links section: Add personal account URLs for Wantedly, YOUTRUST, Qiita, and Zenn

--- components/sections/LinksSection.tsx
+++ components/sections/LinksSection.tsx
@@ -15,7 +15,7 @@ const links = [
   {
     name: 'Wantedly',
     icon: <SiWantedly className="w-16 h-16" />,
-    url: 'https://www.wantedly.com',
+    url: 'https://www.wantedly.com/id/keisuke_hirabayashi1',
     color: '#00A4BB',
   },
   {
@@ -30,19 +30,19 @@ const links = [
         <path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm-1 17.93c-3.95-.49-7-3.85-7-7.93 0-.62.08-1.21.21-1.79L9 15v1c0 1.1.9 2 2 2v1.93zm6.9-2.54c-.26-.81-1-1.39-1.9-1.39h-1v-3c0-.55-.45-1-1-1H8v-2h2c.55 0 1-.45 1-1V7h2c1.1 0 2-.9 2-2v-.41c2.93 1.19 5 4.06 5 7.41 0 2.08-.8 3.97-2.1 5.39z" />
       </svg>
     ),
-    url: 'https://youtrust.jp',
+    url: 'https://youtrust.jp/users/971e265cff5d2ffd723919b5603a7696',
     color: '#2f80ed',
   },
   {
     name: 'Qiita',
     icon: <SiQiita className="w-16 h-16" />,
-    url: 'https://qiita.com',
+    url: 'https://qiita.com/hamburger39', 
     color: '#55c500',
   },
   {
     name: 'Zenn',
     icon: <SiZenn className="w-16 h-16" />,
-    url: 'https://zenn.dev',
+    url: 'https://zenn.dev/k_hirabayashi',
     color: '#3ea8ff',
   },
 ]
